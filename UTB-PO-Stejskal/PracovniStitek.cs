d4dab4abf32696157af2317a45930eb16f9308d3
ostejskal <ostejskal@OSTEJSKAL-UB>
2021-09-04 22:06:11 +0200

    dsfsdfsd

--- UTB-PO-Stejskal/PracovniStitek.cs
+++ UTB-PO-Stejskal/PracovniStitek.cs
@@ -7,11 +7,20 @@ using System.Threading.Tasks;
 namespace UTB_PO_Stejskal
 {
     enum TypPracStitek { prednaska, cviceni, seminar, zapocet, klasifikovany_zapocet, zkouska }
+    enum JazykStitek { cz , eng}
     class PracovniStitek
     {
         String Nazev;
         Zamestnanec Zamestnanec = new Zamestnanec();
         Predmet Predmet = new Predmet();
         TypPracStitek typ;
+        int PocetStudentu;
+        int PocetHodin;
+        int PocetTydnu;
+        JazykStitek JazykStitek;
+        public int PocetBoduZaPracovniStitek ()
+        {
+            return 0;
+        }
     }
 }
---
+++ UTB-PO-Stejskal/VahyPracovnichBodu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTB_PO_Stejskal
+{
+    class VahyPracovnichBodu
+    {
+        double HodinaPrednasky;
+        double HodinaCviceni;
+        double HodinaSeminare;
+        double HodinaPrednaskyAnglicky;
+        double HodinaCviceniAnglicky;
+        double HodinaSeminareAnglicky;
+        double UdeleniJednohoZapoctu;
+        double UdeleniJednohoKlasifikovanehoZapoctu;
+        double UdeleniJedneZkousky;
+        double UdeleniJednohoZapoctuAnglicky;
+        double UdeleniJednohoKlasifikovanehoZapoctuAnglicky;
+        double UdeleniJedneZkouskyAnglicky;
+    }
+}
