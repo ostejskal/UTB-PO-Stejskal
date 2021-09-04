608b08c0d9922c580d8661cb71a9b3cd354f0485
ostejskal <ostejskal@OSTEJSKAL-UB>
2021-09-04 21:08:07 +0200

    update

---
+++ UTB-PO-Stejskal/Predmet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTB_PO_Stejskal
+{
+    class Predmet
+    {
+        String Zkratka;
+        int PocetTydnu = 14;
+        int HodinyPrednasek;
+        int HodinyCviceni;
+        int HodinySeminaru;
+        enum ZpusobZakonceni { z, zk}
+        enum Jazyk { cz, eng}
+        int VelikostTridy = 24;
+        List<Skupinka> SeznamSkupin;
+        String NazevPredmetu;
+        int PocetKreditu;
+        enum GarantujiciUstav { UIUI, FAI}
+        String JmenoGaranta;
+    }
+}
---
+++ UTB-PO-Stejskal/Skupinka.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTB_PO_Stejskal
+{
+    class Skupinka
+    {
+        string Zkratka;
+        int Rocnik;
+        enum Semestr { LS, ZS }
+        int PocetStudentu;
+        enum FormaStudia { P, K}
+        enum TypStudia { Bc , Mgr  }
+        enum Jazyk { cz , eng }
+        String Nazev;
+        List<Predmet> SeznamPredmetu;
+    }
+}
