3aac499736894ca70c5bae7baf1072db186e1094
ostejskal <ostejskal@OSTEJSKAL-UB>
2021-09-11 08:53:26 +0200

    XML allobjects v jednom souboru

--- UTB-PO-Stejskal/TvorbaStudijniSkupiny.cs
+++ UTB-PO-Stejskal/TvorbaStudijniSkupiny.cs
@@ -12,18 +12,28 @@ namespace UTB_PO_Stejskal
 {
     public partial class TvorbaStudijniSkupiny : Form
     {
+        public AllObjects allObjects;
         public TvorbaStudijniSkupiny()
         {
             InitializeComponent();
+            XMLObject obj = new XMLObject();
+            allObjects = obj.DeSerialize();
         }
 
         private void oktvorbaskupiny_Click(object sender, EventArgs e)
         {
-            Skupinka novaskupinka = new Skupinka();
-            novaskupinka.Zkratka = textBox9.Text;
-            novaskupinka.Rocnik = int.Parse(textBox8.Text);
-            novaskupinka.PocetStudentu = int.Parse(textBox6.Text);
-            novaskupinka.Nazev = textBox1.Text;
+            try
+            {
+                Skupinka novaskupinka = new Skupinka();
+                novaskupinka.Zkratka = textBox9.Text ?? "";
+                if (textBox8.Text != "") novaskupinka.Rocnik = int.Parse(textBox8.Text);
+                if (textBox6.Text != "") novaskupinka.PocetStudentu = int.Parse(textBox6.Text);
+                novaskupinka.Nazev = textBox1.Text ?? "";
+                XMLObject obj = new XMLObject();
+                allObjects.listskupinky.Add(novaskupinka);
+                obj.Serialize(allObjects);
+            }
+            catch (Exception ex) { MessageBox.Show("Chyba při přidávání předmětu: " + ex.Message); }
         }
     }
 }
--- UTB-PO-Stejskal/TvorbaZamestnance.cs
+++ UTB-PO-Stejskal/TvorbaZamestnance.cs
@@ -6,19 +6,23 @@ namespace UTB_PO_Stejskal
 {
     public partial class TvorbaZamestnance : Form
     {
+        public AllObjects allObjects;
         public TvorbaZamestnance()
         {
             InitializeComponent();
+            XMLObject obj = new XMLObject();
+            allObjects = obj.DeSerialize();
         }
 
         private void oktvorbazamestnance_Click(object sender, EventArgs e)
         {
-            Zamestnanec novyzamestnanec = new Zamestnanec();
-            novyzamestnanec.jmeno = textBox5.Text ?? "";
-            novyzamestnanec.prijmeni = textBox4.Text ?? "";
-            novyzamestnanec.pracovniemail = textBox3.Text ?? "";
-            novyzamestnanec.soukromyemail = textBox2.Text ?? "";
-            try {
+            try
+            {
+                Zamestnanec novyzamestnanec = new Zamestnanec();
+                novyzamestnanec.jmeno = textBox5.Text ?? "";
+                novyzamestnanec.prijmeni = textBox4.Text ?? "";
+                novyzamestnanec.pracovniemail = textBox3.Text ?? "";
+                novyzamestnanec.soukromyemail = textBox2.Text ?? "";
                 if (textBox7.Text != "")
                 {
                     novyzamestnanec.doktorand = bool.Parse(textBox7.Text);
@@ -27,13 +31,13 @@ namespace UTB_PO_Stejskal
                 {
                     novyzamestnanec.uvazek = Double.Parse(textBox8.Text);
                 }
+                novyzamestnanec.pracovnitelefon = textBox10.Text ?? "";
+                novyzamestnanec.soukromytelefon = textBox11.Text ?? "";
+                XMLObject obj = new XMLObject();
+                allObjects.listzamestnancu.Add(novyzamestnanec);
+                obj.Serialize(allObjects);
             }
-            catch (DataException)
-            {
-                
-            }
-            novyzamestnanec.pracovnitelefon = textBox10.Text ?? "";
-            novyzamestnanec.soukromytelefon = textBox11.Text ?? "";
-            }
+            catch (Exception ex) { MessageBox.Show("Chyba při přidávání předmětu: " + ex.Message); }
+        }
     }
 }
